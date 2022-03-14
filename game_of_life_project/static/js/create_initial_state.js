7a7ef253e0323f9acdb462748b0520764f08637d
Davids Zakrevskis <2577947z@student.gla.ac.uk>
2022-03-14 16:36:50 +0000

    initial state creation improvements

--- game_of_life_project/static/js/create_initial_state.js
+++ game_of_life_project/static/js/create_initial_state.js
@@ -1,27 +1,86 @@
 const canvas = document.getElementById("state"); // id of canvas must be state
 const ctx = canvas.getContext("2d");
 
+// initialize canvas size
 canvas.width = 0.8 * window.innerWidth;
 canvas.height = canvas.width / 2;
 
-const col_count = 50;
-const row_count = col_count / 2;
-
+// set default git
+var col_count = 100;
+var row_count = col_count / 2;
 var grid_spacing = canvas.width / col_count
-
-// 2d array
-let grid = Array(row_count).fill(null)
+// initial empty grid
+var initial_state = Array(row_count).fill(null)
     .map(() => new Array(col_count).fill(null)
         .map(() => 0)); // Math.floor(Math.random() * 2)
+        
 width_height();
-render(grid, ctx);
+current_state = JSON.parse(JSON.stringify(initial_state));
+render(current_state, grid_spacing);
 
 // Event listeners
+document.getElementById("grid_size").addEventListener("change", function () {
+    var select = document.getElementById('grid_size');
+    var option = select.options[select.selectedIndex];
+    var val = option.value;
+    if (val == "s") {
+        col_count = 50;
+        row_count = 25;
+    } else if (val == "m"){
+        col_count = 100;
+        row_count = 50;
+    } else if (val == "l"){
+        col_count = 200;
+        row_count = 100;
+    } else if (val == "x") {
+        col_count = 300;
+        row_count = 150;
+    }
+            
+    width_height();
+
+    initial_state = Array(row_count).fill(null)
+        .map(() => new Array(col_count).fill(null)
+            .map(() => 0)); // Math.floor(Math.random() * 2)
+    current_state = JSON.parse(JSON.stringify(initial_state));
+    render(current_state, grid_spacing);
+});
+
+document.getElementById("shift_right").addEventListener("click", function () { 
+    for (let row = 0; row < row_count; row++) {
+        current_state[row].pop();
+        current_state[row].unshift(0);
+    }
+    render(current_state, grid_spacing);
+});
+document.getElementById("shift_left").addEventListener("click", function () { 
+    for (let row = 0; row < row_count; row++) {
+        current_state[row].shift();
+        current_state[row].push(0);
+    }
+    render(current_state, grid_spacing);
+});
+document.getElementById("shift_down").addEventListener("click", function () { 
+    current_state.pop()
+    current_state.unshift(Array(col_count).fill(0))
+    render(current_state, grid_spacing);
+});
+document.getElementById("shift_up").addEventListener("click", function () { 
+    current_state.shift()
+    current_state.push(Array(col_count).fill(0))
+    render(current_state, grid_spacing);
+});
+
+
 document.getElementById("set").addEventListener("click", function () { 
-    console.log('set!');
+    initial_state = JSON.parse(JSON.stringify(current_state));
+    render(current_state, grid_spacing);
 });
 document.getElementById("return").onclick = function () { 
-    console.log('return!');
+    console.log(current_state[0]);
+    console.log(initial_state[0]);
+    current_state = JSON.parse(JSON.stringify(initial_state));
+    render(current_state, grid_spacing);
 };
 document.getElementById("play").onclick = function () { 
     console.log('play!');
@@ -30,16 +89,24 @@ document.getElementById("pause").onclick = function () {
     console.log('pause!');
 };
 document.getElementById("clear").onclick = function () { 
-    console.log('clear!');
+    current_state = Array(row_count).fill(null)
+    .map(() => new Array(col_count).fill(null)
+        .map(() => 0));
+        render(current_state, grid_spacing);
 };
 document.getElementById("invert").onclick = function () { 
-    console.log('invert!');
+    for (let row = 0; row < row_count; row++) {
+        for (let col = 0; col < col_count; col++){
+            current_state[row][col] = current_state[row][col] ? 0 : 1;
+        }
+    }
+    render(current_state, grid_spacing);
 };
 
 window.addEventListener('resize', 
     function () {
         width_height();
-        render(grid, ctx);
+        render(current_state, grid_spacing);
     }
 );
 canvas.addEventListener('click', 
@@ -51,14 +118,14 @@ canvas.addEventListener('click',
         var col = Math.floor(x / grid_spacing);
         var row = Math.floor(y / grid_spacing);
         console.log(x, y,grid_spacing)
-        grid[row][col] = grid[row][col] ? 0 : 1;
-        render(grid, ctx);
+        current_state[row][col] = current_state[row][col] ? 0 : 1;
+        render(current_state, grid_spacing);
     }
 )
 window.addEventListener('keyup', event => {
     if (event.code === 'Space') {
-        grid = next_generation(grid)
-        render(grid);
+        current_state = next_generation(current_state)
+        render(current_state, grid_spacing);
     }
 });
 
@@ -68,6 +135,7 @@ function width_height() {
     canvas.height = canvas.width / 2;
     grid_spacing = canvas.width / col_count
 }
+
 function render(grid) {
     // ctx.restore()
     for (let row = 0; row < row_count; row++) {
@@ -79,6 +147,7 @@ function render(grid) {
         }
     }
 }
+
 function next_generation(grid) {
     const next_gen = grid.map(arr => [...arr]);
 
